8ba12f2c65fde75fd58e49a2857ad2681070203d
jimmy <py890000@gmail.com>
2018-09-26 11:33:07 +0800

    添加注册服务器

---
+++ eureka/src/main/java/com/py890000/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.py890000.cloud.eureka;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
--- getway/src/main/java/com/py890000/cloud/getway/GetwayApplication.java
+++ getway/src/main/java/com/py890000/cloud/getway/GetwayApplication.java
@@ -2,13 +2,31 @@ package com.py890000.cloud.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+//@Import(AdditionalRoutes.class)
 public class GetwayApplication {
+//	@Bean
+//	public RouteLocator customRouteLocator(ThrottleGatewayFilter throttle) {
+//		return Routes.locator()
+//					 .route("test")
+//					 .uri("http://httpbin.org:80")
+//					 .predicate(host("**.abc.org").and(path("/image/png")))
+//					 .addResponseHeader("X-TestHeader", "foobar")
+//					 .and()
+//					 .route("test2")
+//					 .uri("http://httpbin.org:80")
+//					 .predicate(path("/image/webp"))
+//					 .add(addResponseHeader("X-AnotherHeader", "baz"))
+//					 .and()
+//					 .build();
+//	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetwayApplication.class, args);
 	}
+
+
+
 }
